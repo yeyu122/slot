2355740752548ae2de9a1f41b44e453f072d1461
yeyu122 <2771262114@qq.com>
2022-11-05 23:47:34 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <HelloWorld msg="msg props 第二栏">
+    <!-- v-slot="slotProps" 作用域插槽 -->
+    <!-- <template v-slot:footer>  具名插槽-->
+      <template #main="slotProps">
+      <span>
+        <h1>具名作用域插槽</h1>
+      <ul>
+        <li v-for="(item, index) in slotProps.list1" :key="index">{{item}}</li>
+      </ul>
+        <p class='red'>222222</p>
+      </span>
+    </template>
+    <template #footer>
+      <p>v-slot:footer 与 #main具名插槽</p>
+      <a href="">111111111111</a>
+      <p class='red'>333333</p>
+    </template>
+  </HelloWorld>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default { 
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.red {
+  height:25px;
+  width:250px;
+  background-color:red;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+    <h1>你好</h1>
+    <h2>{{msg}}</h2>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">{{item}}</li>
+    </ul>
+    <main>
+      <slot srgh='fffff' name='main' :list1="list1"></slot>
+    </main>
+    <footer>
+      <slot name="footer"></slot>
+    </footer>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      list:["标题1","标题2","标题3","标题4","标题5","标题6"],
+      list1:["标题221","标题222","标题223","标题224","标题225","标题226"]
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.red {
+  height:25px;
+  width:250px;
+  background-color:red;
+}
+</style>
